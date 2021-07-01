b3318b823771a33caa8c837a6ecef77db9c6e25b
thimira nirmal <timnirmal@gmail.com>
2021-07-01 22:24:59 +0530

    Supply & Staff Finished

--- src/project/frames/supply/SupplyInternational.java
+++ src/project/frames/supply/SupplyInternational.java
@@ -1,4 +1,17 @@
 package project.frames.supply;
 
+import javafx.event.ActionEvent;
+
 public class SupplyInternational {
+    public void saveItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void pickDepartureDate(ActionEvent actionEvent) {
+    }
+
+    public void pickArrivalDate(ActionEvent actionEvent) {
+    }
+
+    public void addSupplyOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/project/frames/supply/SupplyMain.java
+++ src/project/frames/supply/SupplyMain.java
@@ -1,4 +1,11 @@
 package project.frames.supply;
 
+import javafx.event.ActionEvent;
+
 public class SupplyMain {
+    public void localSupplyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void internationalSupplyOnAction(ActionEvent actionEvent) {
+    }
 }
