2865e85c9a46008f0c2df6d8094fc819ba68f979
thimira nirmal <timnirmal@gmail.com>
2021-07-02 11:39:59 +0530

    Supply changed

--- src/project/frames/supply/SupplyInternational.java
+++ src/project/frames/supply/SupplyInternational.java
@@ -1,17 +1,17 @@
 package project.frames.supply;
 
-import javafx.event.ActionEvent;
+        import javafx.event.ActionEvent;
 
 public class SupplyInternational {
     public void saveItemOnAction(ActionEvent actionEvent) {
     }
 
-    public void pickDepartureDate(ActionEvent actionEvent) {
-    }
-
-    public void pickArrivalDate(ActionEvent actionEvent) {
-    }
-
     public void addSupplyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void pickharbourArrivalDate(ActionEvent actionEvent) {
+    }
+
+    public void pickstoreArrivalDate(ActionEvent actionEvent) {
     }
 }
