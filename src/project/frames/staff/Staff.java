86bcf921b0d044441530f797ff4625b6f1b3ee4e
Inventory_System_CO2203 <apmaduwantha@gmail.com>
2021-07-08 09:58:41 +0530

    add staff, dashboard

--- src/project/frames/staff/Staff.java
+++ src/project/frames/staff/Staff.java
@@ -1,15 +1,13 @@
 package project.frames.staff;
-import project.frames.Dashboard;
-import project.welcome_screen.welcome;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javax.swing.*;
 
-class Staff{
-     void f(){
-        System.out.println("sdfdf");
-    }
-    //private String staffName, staffId;
+public class Staff{
+     private String staffName, staffId, userCategory, pasword, joinDate;
+
+     public String getUserCategory() {
+          return userCategory;
+     }
+
+     public void setUserCategory(String userCategory) {
+          this.userCategory = userCategory;
+     }
 }
